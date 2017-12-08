c4af6068b61833cb5ebe8b6412647b21e148ef39
Tomaž Lunder <tomazlunder1@gmail.com>
2017-12-08 16:22:00 +0100

    Učna in testna množica

--- skripte/atributi.R
+++ skripte/atributi.R
@@ -1,6 +1,6 @@
 source("skripte//main.R");
 
-#2. OCENJEVANJE IN PREDELAVA ATRIBUTOV
+#2. OCENJEVANJE IN PREDELAVA ATRIBUTOV, DELJENJE NA UČNO IN TESTNO MNOŽICO
 
 
 #Vrstice z PM10 manjšim od nič (nepravilne meritve - mogoče bo treba odstranitihist), O3 so use ok
@@ -14,6 +14,7 @@ source("skripte//main.R");
 
 vec_doy <- c();
 vec_dow <- c();
+vec_year <- c();
 
 vec_o3_klas <- c();
 vec_pm10_klas <- c();
@@ -21,12 +22,17 @@ vec_pm10_klas <- c();
 for(i in c(1:nrow(table)))
 {
   datum <- table[i,1];
+  
   doy_s <- strftime(datum, format("%j"));
   dow_s <- strftime(datum, format("%w"));
   doy <- as.numeric(doy_s);
   dow <- as.numeric(dow_s) +1;
   vec_doy[i] <- doy;
   vec_dow[i] <- dow;
+  
+  year <- substring(datum,0,4);
+  year <- as.numeric(year);
+  vec_year[i] <- year;
   
   o3 <- table[i,27];
   
@@ -40,7 +46,7 @@ for(i in c(1:nrow(table)))
     vec_o3_klas[i] <- 3;
   }
   
-  pm10 <- table[i,26];
+  pm10 <- table[i,26];plot
 
   
   if(pm10 < 35){
@@ -50,24 +56,43 @@ for(i in c(1:nrow(table)))
   }
 }
 
+#Doda nove stolpce
 table <- cbind(table, vec_doy);
 table <- cbind(table, vec_dow);
 table <- cbind(table, vec_o3_klas);
 table <- cbind(table, vec_pm10_klas);
+table <- cbind(table, vec_year);
 
-#Odstrani datum
-table <- table[,-1];
-
-table = table[,c(27,28,1:24,26,29,25,30)]
+#Preuredi stolpce
+table = table[,c(1,32,28,29,2:25,27,30,26,31)]
 
 #Preimenuje stolpca
+names(table)[names(table) == "vec_pm10_klas"] = "PM10R";
+names(table)[names(table) == "o3"] = "O3";
 names(table)[names(table) == "vec_doy"] = "DOY";
 names(table)[names(table) == "vec_dow"] = "DOW"; 
 names(table)[names(table) == "vec_o3_klas"] = "O3R";
 names(table)[names(table) == "vec_pm10_klas"] = "PM10R";
+names(table)[names(table) == "vec_year"] = "Leto";
 
-write.csv(table,file = "podatki//obdelani//obdelano1.csv");
+#write.csv(table,file = "podatki//obdelani//obdelano1.csv");
+#table2 <- read.table("podatki//obdelani//obdelano1", T, ",");
+#table2 <- table2[,-1];
 
-table2 <- read.table("podatki//obdelani//obdelano1", T, ",");
-table2 <- table2[,-1];
+#Odstranimo datum (ne potrebujemo več)
+table <- table[,-1]
+
+#DELJENJE NA TESTNO IN UČNO MNOŽICO
+brez2016 <- table[table$Leto != 2016,];
+table2016 <- table[table$Leto == 2016,];
+
+table2016mix <- table2016[sample(1:nrow(table2016)), ];
+
+test <- table2016mix[c(1:496),];
+back_to_learn <- table2016mix[c(497:nrow(table2016mix)), ];
+
+learn <- rbind(brez2016, back_to_learn);
+
+write.csv(learn,file = "podatki//obdelani//learn.csv");
+write.csv(test,file = "podatki//obdelani//test.csv");
 
--- skripte/vizualizacija.R
+++ skripte/vizualizacija.R
@@ -10,5 +10,28 @@ hist(table$O3);
 boxplot(table$PM10, horizontal = TRUE);
 boxplot(table$O3, horizontal = TRUE);
 
-nrow()
+# PIE CHARTI PORAZDELITEV PO RAZREDIH ZA
+# - o3
+slices <- c(nrow(table[table$O3R == 0,]), nrow(table[table$O3R == 1,]),nrow(table[table$O3R == 2,]),nrow(table[table$O3R == 3,]));
+labels <- c("NIZKA","SREDNJA", "VISOKA", "EKSTREMNA");
+pct <- round(slices/sum(slices)*100,3);
+labels <- paste(labels, pct);
+
+pie(slices, labels);
+# - pm10
+slices <- c(nrow(table[table$PM10R == 0,]), nrow(table[table$PM10R == 1,]));
+labels <- c("NIZKA", "VISOKA");
+pct <- round(slices/sum(slices)*100,3);
+labels <- paste(labels, pct);
+
+pie(slices, labels);
+
+pieo3 <- function (table){
+slices <- c(nrow(table[table$O3R == 0,]), nrow(table[table$O3R == 1,]),nrow(table[table$O3R == 2,]),nrow(table[table$O3R == 3,]));
+labels <- c("NIZKA","SREDNJA", "VISOKA", "EKSTREMNA");
+pct <- round(slices/sum(slices)*100,3);
+labels <- paste(labels, pct);
+
+pie(slices, labels);
+}
 
